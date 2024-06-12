9abe0676d3337b77b8be1f711de392c4e53794c2
xfp23 <120090946+xfp23@users.noreply.github.com>
2024-06-13 02:36:43 +0800

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,10 +1,17 @@
+package snippet;
 public class P1 {
-    public static void main(String[]args){
-        StringBuffer sb=new StringBuffer("Listen");
+    public static void main(String[] args) {
+    	String add_str = "Me";
+    	String mid_str = "To";
+    	String init_str = "Listen";
+    	P2 p2 = new P2();
+        StringBuffer sb = new StringBuffer(init_str);
         System.out.println(sb.toString());
-        sb.append("Me!" );
+        sb.append(add_str);
         System.out.println(sb.toString());
-        sb.insert(6,"To");
+        sb.insert(init_str.length(), mid_str);
         System.out.println(sb.toString());
+        System.out.println(p2.judge());
+        P3.print_json();
     }
 }
